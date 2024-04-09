6bcbb131177ac6054aa82fe84f2a0c32435676c1
Eldar Gasanov @kryst4l320 <n1k3f1t@gmail.com>
2024-04-09 11:44:27 +0300

    firmware update

--- firmware/v1.ino
+++ firmware/v1.ino
@@ -1,255 +1,317 @@
-//Libraries
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
 #include <EEPROM.h>
-//Constants
-ESP8266WebServer  server(80)
+#include <FastLEDHub.h>
 
-// Data structures
-struct settings {
-  char ssid[30];
-  char password[30];
-  int  pc_id[30];
-} user_settings = {};
+#define LED_PIN     2
+#define NUM_LEDS    30
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+#define MAX_RETRIES 5
+#define RETRY_DELAY 2000
+#define FASTLED_ESP8266_RAW_PIN_ORDER
+#define FASTLED_ESP8266_NODEMCU_PIN_ORDER
+#define LIGHTSTRIP_PIN 2
 
-void setup() {
-  EEPROM.begin(sizeof(struct settings));
+// structures setup
 
-  WiFi.mode(WIFI_AP);
-  WiFi.softAp("Setup portal", "hq");
+struct {
+  char id[30];
+  char wiFiSsid[30];
+  char wiFiPassword[30];
+  char targetId[30];
+  char ipAddress[30];
+  char port[30];
+  char ledStripOnColor[30];
+  char ledStripOffColor[30];
+  char active[1];
+} deviceSetup;
 
-  server.on("/", handlePortal);
+// common entities
+
+ESP8266WebServer  server(80);
+WiFiClient client;
+HTTPClient http;
+CRGB leds[NUM_LEDS];
+
+// basic functions
+void pinoutSetup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void startSerialCommunication() {
+  Serial.begin(115200);
+  delay(300);
+}
+
+void blinkLed(int interval) {
+  digitalWrite(LED_PIN, HIGH);
+  delay(interval);
+  digitalWrite(LED_PIN, LOW);
+  delay(interval);
+}
+
+// led setup
+void setupLed() {
+  FastLEDHub.addLeds<LED_TYPE, LIGHTSTRIP_PIN, GRB>(leds, NUM_LEDS);
+}
+
+void paintLedStrip() {
+  Serial.println(bool(deviceSetup.active));
+  if (bool(deviceSetup.active)) {
+    fill_solid(leds, NUM_LEDS, CRGB::Green);
+  } else {
+    fill_solid(leds, NUM_LEDS, CRGB::Red);
+  }
+  FastLEDHub.show();
+  delay(1000);
+}
+
+// eeprom workaround
+
+void initEeprom() {
+  EEPROM.begin(sizeof(deviceSetup));
+  EEPROM.get(0, deviceSetup);
+
+  Serial.println("Read from EEPROM:");
+
+  Serial.print("WiFi SSID: ");
+  Serial.println(deviceSetup.wiFiSsid);
+  Serial.print("WiFi password: ");
+  Serial.println(deviceSetup.wiFiPassword);
+
+  Serial.print("ID: ");
+  Serial.println(deviceSetup.id);
+
+  Serial.print("Server ip: ");
+  Serial.println(deviceSetup.ipAddress);
+  Serial.print("Server port: ");
+  Serial.println(deviceSetup.port);
+
+  Serial.print("Target id: ");
+  Serial.println(deviceSetup.targetId);
+
+  Serial.print("Led strip on color: ");
+  Serial.println(deviceSetup.ledStripOnColor);
+  Serial.print("Led strip off color: ");
+  Serial.println(deviceSetup.ledStripOffColor);
+}
+
+// wifi setup
+
+void connectWiFi() {
+  WiFi.mode(WIFI_STA);
+  delay(100);
+
+  Serial.print("Connecting to WiFi ");
+
+  for (int attempt = 0; attempt < MAX_RETRIES; ++attempt) {
+    WiFi.begin(deviceSetup.wiFiSsid, deviceSetup.wiFiPassword);
+    Serial.print(".");
+
+    unsigned long startAttemptTime = millis();
+    while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < RETRY_DELAY) {
+      delay(100);
+    }
+
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println();
+      Serial.println("WiFi connected successfully!");
+      Serial.print("IP Address: ");
+      Serial.println(WiFi.localIP());
+      return;
+    } else {
+      Serial.print(" (Attempt ");
+      Serial.print(attempt + 1);
+      Serial.print("/");
+      Serial.print(MAX_RETRIES);
+      Serial.print(" failed)");
+
+      if (attempt < MAX_RETRIES - 1) {
+        Serial.print(", retrying in ");
+        Serial.print(RETRY_DELAY / 1000);
+        Serial.println(" seconds...");
+        delay(RETRY_DELAY);
+      } else {
+        Serial.println();
+        Serial.println("Failed to connect to WiFi.");
+
+        // Create WiFi network with unique ID (MAC address)
+        Serial.println("Falling back to AP mode.");
+        WiFi.disconnect();
+        WiFi.mode(WIFI_AP);
+        delay(100);
+
+        // Check if unique_id exists in user_settings
+        String uniqueSSID = "ESP32_";
+        if (sizeof(deviceSetup.id) > 2) {
+          uniqueSSID += deviceSetup.id;
+        } else {
+          uniqueSSID += WiFi.macAddress();
+        }
+        WiFi.softAP(uniqueSSID.c_str());
+        Serial.print("WiFi access point network created. SSID: ");
+        Serial.println(uniqueSSID);
+        Serial.print("IP Address (AP mode): ");
+        Serial.println(WiFi.softAPIP());
+        return;
+      }
+    }
+  }
+}
+
+// web ui for initial setup
+
+String settingsHtmlForm = R"(
+<!DOCTYPE html>
+<html lang='ru'>
+  <head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Configuration Form</title>
+  </head>
+  <body>
+    <h2>Configuration Form</h2>
+    <form id='configForm' method='post' action='/save-settings'>
+      <label for='wifiSSID'>WiFi SSID:</label><br>
+      <input type='text' id='wifiSSID' name='wifiSSID' required><br>
+
+      <label for='wifiPassword'>WiFi Password:</label><br>
+      <input type='password' id='wifiPassword' name='wifiPassword' required><br>
+
+      <label for='id'>ID:</label><br>
+      <input type='text' id='id' name='id' required><br>
+
+      <label for='targetId'>Target host ID:</label><br>
+      <input type='text' id='targetId' name='targetId' required><br>
+
+      <label for='serverIP'>Server IP Address:</label><br>
+      <input type='text' id='serverIP' name='serverIP' required><br>
+
+      <label for='serverPort'>Server Port:</label><br>
+      <input type='number' id='serverPort' name='serverPort' required><br><br>
+
+      <input type='submit' value='Submit'>
+    </form>
+    <button id="blink-button">Blink led</button>
+  </body>
+  <script>
+    const button = document.getElementById('blink-button');
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      fetch('/blink', {
+        method: 'POST',
+      });
+    });
+  </script>
+</html>
+)";
+
+// http client
+void httpHostStatusRequest() {
+  if (deviceSetup.targetId[0] == '\0' || deviceSetup.ipAddress[0] == '\0') {
+    return;
+  }
+
+  String URL = "http://" + String(deviceSetup.ipAddress) + ":" + String(deviceSetup.port) + "/hosts/" + String(deviceSetup.targetId) + "/status";
+
+  http.begin(client, URL);
+
+  int httpCode = http.GET();
+
+  if (httpCode == HTTP_CODE_OK) {
+    String payload = http.getString();
+    payload.toCharArray(deviceSetup.active, sizeof(deviceSetup.active));
+    Serial.print("Status:");
+    Serial.print(payload);
+    Serial.print(" Target:");
+    Serial.print(deviceSetup.targetId);
+    Serial.print(" active:");
+    Serial.print(deviceSetup.active);
+  } else {
+    Serial.printf("[HTTP] ... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+
+  delay(1000);
+}
+
+// web server
+
+void fetchActiveStatus() {
+
+}
+
+void handesSettingsPageVisit() {
+  server.send(200, "text/html", settingsHtmlForm);
+}
+
+void handleBlinkButtonCall() {
+  Serial.println("Blink request");
+  for (int i = 0; i < 10; i++) {
+    blinkLed(300);
+  }
+  server.send(200);
+}
+
+void handleSaveSettingsReq() {
+  String ssid = server.arg("wifiSSID");
+  String password = server.arg("wifiPassword");
+
+  String uniqueId = server.arg("id");
+  String targetId = server.arg("targetId");
+
+  String serverIpAddress = server.arg("serverIP");
+  String serverPort = server.arg("serverPort");
+
+  uniqueId.toCharArray(deviceSetup.id, sizeof(deviceSetup.id));
+  targetId.toCharArray(deviceSetup.targetId, sizeof(deviceSetup.targetId));
+
+  ssid.toCharArray(deviceSetup.wiFiSsid, sizeof(deviceSetup.wiFiSsid));
+  password.toCharArray(deviceSetup.wiFiPassword, sizeof(deviceSetup.wiFiPassword));
+
+  serverIpAddress.toCharArray(deviceSetup.ipAddress, sizeof(deviceSetup.ipAddress));
+  serverPort.toCharArray(deviceSetup.port, sizeof(deviceSetup.port));
+
+  EEPROM.put(0, deviceSetup);
+  EEPROM.commit();
+
+  server.send(200, "text/plain", "Settings saved");
+  delay(200);
+
+  ESP.restart();
+}
+
+void setupWebServer() {
+  server.on("/", HTTP_GET, handesSettingsPageVisit);
+  server.on("/save-settings", HTTP_POST, handleSaveSettingsReq);
+  server.on("/blink", HTTP_POST, handleBlinkButtonCall);
   server.begin();
 }
 
+
+void setup() {
+  http.addHeader("Content-Type", "application/json");
+  pinoutSetup();
+  startSerialCommunication();
+  setupLed();
+
+  for (int i = 0; i < 3; i++) {
+    blinkLed(300);
+  }
+
+  initEeprom();
+
+  connectWiFi();
+  setupWebServer();
+}
+
 void loop() {
-  server.handleClient()
+  server.handleClient();
+  httpHostStatusRequest();
+  paintLedStrip();
+  FastLEDHub.handle();
 }
-
-void handlePortal() {
-  server.on("/", {
-    server.send(200, "text/html", wifiSettingsPage)
-  });
-  server.on("/save-network", {
-    strncpy(user_settings.ssid, server.arg("ssid").c_str(), sizeof(user_settings.ssid));
-    strncpy(user_settings.password, server.arg("password").c_str(), sizeof(user_settings.ssid));
-  });
-}
-
-// Html code
-String wifiSettingsPage =  R"(<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>NodeMCU WiFi setup</title>
-</head>
-<body class="body">
-  <style>
-    *,
-    ::before,
-    ::after {
-      box-sizing: border-box;
-      margin: 0;
-      padding: 0;
-    }
-    a {
-      color: inherit;
-      text-decoration: inherit;
-    }
-    h1,
-    h2,
-    h3 {
-      margin: 0;
-    }
-    html {
-      height: 100dvh;
-      width: 100%;
-    }
-    .container {
-      max-width: 1280px;
-      padding: 0 2em;
-      margin: auto;
-    }
-    .body {
-      margin: 0;
-      padding: 0;
-      position: relative;
-      min-height: 100dvh;
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      font-family: Arial, Helvetica, sans-serif;
-      font-size: 12px;
-      line-height: 120%;
-      background-color: darkslategray;
-      color: white;
-    }
-    .header {
-      padding: 1.5em 2em;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: sticky;
-      top: 0;
-      left: 0;
-      flex: 0 1 auto;
-      max-height: max-content;
-      border-bottom: 1px solid white;
-      backdrop-filter: blur(3px);
-    }
-    .links-list {
-      display: flex;
-      align-items: center;
-      margin: 0;
-      list-style: none;
-      width: 100%;
-      font-size: 1rem;
-    }
-    .links-list li {
-      height: 100%;
-      padding: 0 2em;
-      position: relative;
-    }
-    .links-list li:not(:last-child)::after {
-      position: absolute;
-      top: 0;
-      right: 0;
-      content: "";
-      height: 100%;
-      width: 1px;
-      background-color: white;
-    }
-    .main {
-      display: flex;
-      flex-direction: column;
-      flex: 1 1 100%;
-    }
-    .settings,
-    .contacts {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      flex: 1 1 100%;
-      width: 100%;
-      min-height: 92dvh;
-      margin-bottom: 100px;
-    }
-    .contacts {
-      justify-content: flex-start;
-      margin-bottom: 0;
-    }
-    .settings__title,
-    .contacts__title {
-      text-align: center;
-      font-weight: normal;
-      margin: 1em 0;
-    }
-    .settings__form {
-      display: flex;
-      flex-direction: column;
-      flex: 0 1;
-    }
-    .settings__input-wrapper {
-      padding: 0.5rem 0;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      width: 100%;
-      font-size: 1rem;
-      margin: auto;
-    }
-    .settings__input-wrapper > label {
-      font-size: 0.8rem;
-      color: lightgray;
-    }
-    .settings__input-wrapper > input {
-      margin: 0.5em 0;
-      padding: 0.5em 1em;
-      background-color: inherit;
-      border: none;
-      outline: 1px solid white;
-      border-radius: 4px;
-      color: white;
-    }
-    .settings__submit-btn {
-      margin: 2em 0;
-      padding: 0.5em 1em;
-      display: block;
-      background-color: transparent;
-      border: 1px solid white;
-      border-radius: 4px;
-      color: white;
-      flex: 0 1 auto;
-    }
-    .contacts__title {
-      margin-top: 1rem;
-      margin-bottom: 3rem;
-      font-size: 1.2rem;
-    }
-    .contacts__links-list {
-      margin: 0;
-      list-style: none;
-      display: flex;
-      flex-direction: column;
-      gap: 1em;
-      font-size: 1.2rem;
-    }
-    .contacts__link-name {
-      font-size: 1rem;
-      margin-right: 10px;
-      color: lightgray;
-    }
-  </style>
-  <header class="header">
-    <nav class="container">
-      <ul class="links-list">
-        <li>
-          <a href="#settings">
-            Настройки
-          </a>
-        </li>
-        <li>
-          <a href="#contacts">
-            Контакты
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </header>
-  <main class="main">
-    <section id="settings" class="settings container">
-      <form class="settings__form" action="/save-network" method="post">
-        <h2 class="settings__title">Настройка WiFi</h2>
-        <div class="settings__input-wrapper">
-          <label for="ssid">Название</label>
-          <input autocomplete="on" tabindex="0" id="ssid" type="text">
-        </div>
-        <div class="settings__input-wrapper">
-          <label for="password">Пароль</label>
-          <input autocomplete="on" type="password" id="password" type="text">
-        </div>
-        <button class="settings__submit-btn" type="submit">Сохранить</button>
-      </form>
-    </section>
-    <section id="contacts" class="contacts container">
-      <h2 class="contacts__title">Контакты</h2>
-      <ul class="contacts__links-list">
-        <li>
-          <p>
-            <span class="contacts__link-name">TG:</span>
-            <a href="https://telegram.me/kryst4l320" target="_blank">@kryst4l320</a>
-          </p>
-        </li>
-        <li>
-          <p>
-            <span class="contacts__link-name">GitHub</span>
-            <a href="https://github.com/hrustalq" target="_blank">hrustalq</a>
-          </p>
-        </li>
-      </ul>
-    </section>
-  </main>
-</body>
-</html>
-)";
