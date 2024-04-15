b57ec05a5b93014dba5b400f458c916be69f34d2
hrustalq <n1k3f1t@gmail.com>
2024-04-15 10:19:13 +0300

    temporarely remove frontend part

--- fe/src/api/hosts/get-all-hosts.ts
+++
@@ -1,5 +0,0 @@
-import { apiInstance } from "../instance"
-
-export default async () => {
-  return await apiInstance.get("/hosts/all").then((response) => response.data);
-}
--- fe/src/api/hosts/index.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  async getAllHosts() {
-    return await import("./get-all-hosts").then((module) => module.default());
-  }
-}
